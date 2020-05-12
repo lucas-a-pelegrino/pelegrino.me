af4a3f69599c8383fa299c7670b7fe5e4eb4b893
Lucas Pelegrino <lucaspelegrino@ioasys.com.br>
2020-05-11 21:26:02 -0300

    refactor: turning some components in presentation view

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,8 @@
 import React from 'react';
-import styled, { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
 
-import { WindowCanvas, WindowBar, WindowFooter } from '../components';
-
-import BackgroundImageUrl from '../assets/images/night-road.jpg';
-
-const BackgroundImage = createGlobalStyle`
-  body {
-    background-image: url(${BackgroundImageUrl});
-    background-size: cover;
-    background-repeat: no-repeat;
-
-    margin: 0;
-  }
-`;
+import GlobalStyle from '../GlobalStyles';
+import AppWindow from '../presentation/AppWindow';
 
 const Content = styled.div`
   display: flex;
@@ -23,28 +12,13 @@ const Content = styled.div`
   align-items: center;
 `;
 
-const WindowContainer = styled.div`
-  display: flex;
-  width: 722px;
-  height: 463px;
-  flex-flow: column;
-  justify-content: center;
-  align-items: center;
-
-  border: solid 1px;
-  border-radius: 5px;
-  box-shadow: inset 0px 0px 0px 1px #808383;
-`;
-
-export default function App() {
+const App = () => {
   return (
     <Content>
-      <WindowContainer>
-        <WindowBar />
-        <WindowCanvas />
-        <WindowFooter />
-      </WindowContainer>
-      <BackgroundImage />
+      <GlobalStyle />
+      <AppWindow />
     </Content>
   );
-}
+};
+
+export default App;
---
+++ src/presentation/AppWindow/AppWindow.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { SideBar } from '../../components';
+
+import {
+  AppWindowStyle as Container,
+  TopBarStyle as TopBar,
+  StreetlightStyle as Streetlight,
+  EditorStyle as Editor,
+  BottomBarStyle as BottomBar,
+  CopyrightTextStyle as CopyrightText,
+} from './styled';
+
+const AppWindow = () => {
+  return (
+    <Container>
+      <TopBar>
+        <Streetlight color="#ff5a52" />
+        <Streetlight color="#e6c02a" />
+        <Streetlight color="#53c22b" />
+      </TopBar>
+      <Editor>
+        <SideBar />
+      </Editor>
+      <BottomBar>
+        <CopyrightText
+          text="COPYRIGHT @ 2020 - LUCAS PELEGRINO"
+          styles={{ fontColor: '#eceff4', fontWeight: 'bold', fontSize: '8px' }}
+        />
+      </BottomBar>
+    </Container>
+  );
+};
+
+export default AppWindow;
