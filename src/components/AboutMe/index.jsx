ae8e8aedf639f84d0dcef6290d7ce213c50737c7
Lucas Pelegrino <lucas.assuncao.p@gmail.com>
2020-05-21 00:28:52 -0300

    style: adjusting about me component

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -10,20 +10,13 @@ const AboutMe = () => {
       <Avatar />
       <AboutMeText>
         Hi! My name is Lucas Pelegrino, 26 years old living in Belo Horizonte - Minas Gerais, Brazil.
-        <br />
-        <br />
-        Currently working as Full Stack Developer at ioasys.
-        <br />
-        <br />
-        I have been writing code for about 4+ years, started out only with Back End but soon expanded my base
-        knowledge on Front End as well, I still believe my expertise lies on the Back End, though. Also, I
-        very much enjoy project management and even had a chance to do so in some projects I was part of at
-        ioasys. Lately, the interest in learning Swift have grown quite a lot on me so I am planning to play
-        around with it, who knows, right?
-        <br />
-        <br />
-        Ah! I also love classic rock like Led Zeppelin and country folks like The White Buffalo, and last but
-        not least important: I am a big fan of Harley Davidson!
+        Currently working as Full Stack Developer at ioasys. I have been writing code for about 4+ years,
+        started out only with Back End but soon expanded my base knowledge on Front End as well, I still
+        believe my expertise lies on the Back End, though. Also, I very much enjoy project management and even
+        had a chance to do so in some projects I was part of at ioasys. Lately, the interest in learning Swift
+        have grown quite a lot on me so I am planning to play around with it, who knows, right? Ah! I also
+        love classic rock like Led Zeppelin and country folks like The White Buffalo, and last but not least
+        important: I am a big fan of Harley Davidson!
       </AboutMeText>
       <Social />
     </Container>
--- src/components/AboutMe/styled.js
+++ src/components/AboutMe/styled.js
@@ -2,12 +2,19 @@ import styled from 'styled-components';
 
 export const AboutMeStyled = styled.div`
   display: flex;
-  align-items: center;
+  height: 100%;
   flex-direction: column;
+  align-items: center;
   justify-content: space-evenly;
+
+  @media (max-width: 768px) {
+  }
 `;
 
-export const AboutMeTextStyled = styled.span`
-  margin: 10px 20px 10px 20px;
+export const AboutMeTextStyled = styled.pre`
+  margin-left: 20px;
+  margin-right: 20px;
   text-align: justify;
+  font-size: 0.99em;
+  white-space: pre-wrap;
 `;
