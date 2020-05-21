ebf2688b8490610c9092f0d1f567feafd129c506
Lucas Pelegrino <lucas.assuncao.p@gmail.com>
2020-05-21 01:09:40 -0300

    style: minor refactorings on about me component

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Social from '../Social';
 import { Avatar } from '../core';
 
-import { AboutMeStyled as Container, Row, AboutMeTextStyled as AboutMeText } from './styled';
+import { AboutMeStyled as Container, Row, AboutMeTextStyled as TextRow } from './styled';
 
 const AboutMe = () => {
   return (
@@ -10,16 +10,16 @@ const AboutMe = () => {
       <Row>
         <Avatar />
       </Row>
-      <AboutMeText>
+      <TextRow>
         Hi! My name is Lucas Pelegrino, 26 years old living in Belo Horizonte - Minas Gerais, Brazil.
         Currently working as Full Stack Developer at ioasys. I have been writing code for about 4+ years,
-        started out only with Back End but soon expanded my base knowledge on Front End as well, I still
-        believe my expertise lies on the Back End, though. Also, I very much enjoy project management and even
-        had a chance to do so in some projects I was part of at ioasys. Lately, the interest in learning Swift
-        have grown quite a lot on me so I am planning to play around with it, who knows, right? Ah! I also
-        love classic rock like Led Zeppelin and country folks like The White Buffalo, and last but not least
+        started out only with Back End but soon expanded my knowledge on Front End as well, I still believe my
+        expertise lies within Back End, though. Also, I very much enjoy project management and even had a
+        chance to do so in some projects I was part of at ioasys. Lately, the interest in learning Swift have
+        grown quite a lot on me so I am planning to play around with it, who knows, right? Ah! I also love
+        classic rock like Led Zeppelin and country folks like The White Buffalo, and last but not least
         important: I am a big fan of Harley Davidson!
-      </AboutMeText>
+      </TextRow>
       <Row>
         <Social />
       </Row>
