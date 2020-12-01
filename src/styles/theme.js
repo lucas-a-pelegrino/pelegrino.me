972403dec4a69e396a7188704c208a1f486096ca
Lucas Pelegrino <lucas.assuncao.p@gmail.com>
2020-12-01 19:27:14 -0300

    refactor: added theming file, moved global styling to styles folder

---
+++ src/styles/theme.js
@@ -0,0 +1,16 @@
+export default {
+  colors: {
+    background: '#434C5E',
+    cardBackground: '#2E3440',
+    cardBorderColor: '#434C5E',
+    text: '#ECEFF4',
+  },
+  font: {
+    familyPrimary: 'Menlo, sans-serif',
+    size: {
+      sm: '11px',
+      md: '11px',
+      lg: '16px',
+    },
+  },
+};
