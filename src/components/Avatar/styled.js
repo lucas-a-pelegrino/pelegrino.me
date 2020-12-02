e8f801d6368366408c271164abda02a444a5c569
Lucas Pelegrino <lucas.assuncao.p@gmail.com>
2020-12-01 21:35:09 -0300

    feat(avatar): increased avatar size to 160x160

--- src/components/Avatar/styled.js
+++ src/components/Avatar/styled.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const AvatarStyle = styled.img`
-  width: 115px;
-  height: 115px;
+  width: 160px;
+  height: 160px;
   box-shadow: 0 4px 15px 0 #0000004c;
   border: 2px solid #4c566a;
   border-radius: 100px;
